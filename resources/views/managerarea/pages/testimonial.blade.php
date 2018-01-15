cdc957149a0a1ae1a4b937ef8d77bbfc6ad36f0b
Abdelrahman Omran <me@omranic.com>
2018-01-15 19:59:32 +0200

    Prevent turbolinks caching pages with datatables

--- resources/views/managerarea/pages/testimonial.blade.php
+++ resources/views/managerarea/pages/testimonial.blade.php
@@ -130,6 +130,10 @@
 
 @if($testimonial->exists)
 
+    @push('head-elements')
+        <meta name="turbolinks-cache-control" content="no-cache">
+    @endpush
+
     @push('styles')
         <link href="{{ mix('css/datatables.css', 'assets') }}" rel="stylesheet">
     @endpush

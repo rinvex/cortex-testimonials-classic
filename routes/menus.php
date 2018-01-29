dc1736475ce395694c9e207290ad10d3047a39f8
Abdelrahman Omran <me@omranic.com>
2018-01-29 16:43:58 +0200

    Apply recent menu updates

--- routes/menus.php
+++ routes/menus.php
@@ -3,15 +3,15 @@
 declare(strict_types=1);
 
 use Rinvex\Menus\Models\MenuItem;
-use Rinvex\Menus\Factories\MenuFactory;
+use Rinvex\Menus\Models\MenuGenerator;
 
-Menu::modify('adminarea.sidebar', function (MenuFactory $menu) {
+Menu::register('adminarea.sidebar', function (MenuGenerator $menu) {
     $menu->findByTitleOrAdd(trans('cortex/foundation::common.crm'), 50, 'fa fa-briefcase', [], function (MenuItem $dropdown) {
         $dropdown->route(['adminarea.testimonials.index'], trans('cortex/testimonials::common.testimonials'), 10, 'fa fa-quote-right')->ifCan('list-testimonials')->activateOnRoute('adminarea.testimonials');
     });
 });
 
-Menu::modify('managerarea.sidebar', function (MenuFactory $menu) {
+Menu::register('managerarea.sidebar', function (MenuGenerator $menu) {
     $menu->findByTitleOrAdd(trans('cortex/foundation::common.crm'), 50, 'fa fa-briefcase', [], function (MenuItem $dropdown) {
         $dropdown->route(['managerarea.testimonials.index'], trans('cortex/testimonials::common.testimonials'), 10, 'fa fa-quote-right')->ifCan('list-testimonials')->activateOnRoute('managerarea.testimonials');
     });

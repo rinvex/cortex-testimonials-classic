cfdd315a6904d77cf1a8a45d5faf6d23299b8730
Abdelrahman Omran <me@omranic.com>
2018-03-31 22:36:10 +0200

    Escape datatable rows

--- src/Transformers/Managerarea/TestimonialTransformer.php
+++ src/Transformers/Managerarea/TestimonialTransformer.php
@@ -4,20 +4,23 @@ declare(strict_types=1);
 
 namespace Cortex\Testimonials\Transformers\Managerarea;
 
+use Rinvex\Support\Traits\Escaper;
 use League\Fractal\TransformerAbstract;
 use Cortex\Testimonials\Models\Testimonial;
 
 class TestimonialTransformer extends TransformerAbstract
 {
+    use Escaper;
+
     /**
      * @return array
      */
     public function transform(Testimonial $testimonial): array
     {
-        return [
+        return $this->escapeRow([
             'id' => (string) $testimonial->getRouteKey(),
             'created_at' => (string) $testimonial->created_at,
             'updated_at' => (string) $testimonial->updated_at,
-        ];
+        ]);
     }
 }

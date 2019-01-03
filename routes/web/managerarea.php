e281fb13ad537e369381f0898cd5efb17a6ea15d
Abdelrahman Omran <me@omranic.com>
2019-01-03 07:33:50 +0200

    Simplify and flatten create & edit form controller actions

--- routes/web/managerarea.php
+++ routes/web/managerarea.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-Route::domain(domain())->group(function () {
-    Route::name('adminarea.')
-         ->namespace('Cortex\Testimonials\Http\Controllers\Adminarea')
-         ->middleware(['web', 'nohttpcache', 'can:access-adminarea'])
-         ->prefix(config('cortex.foundation.route.locale_prefix') ? '{locale}/'.config('cortex.foundation.route.prefix.adminarea') : config('cortex.foundation.route.prefix.adminarea'))->group(function () {
+Route::domain('{subdomain}.'.domain())->group(function () {
+    Route::name('managerarea.')
+         ->namespace('Cortex\Testimonials\Http\Controllers\Managerarea')
+         ->middleware(['web', 'nohttpcache', 'can:access-managerarea'])
+         ->prefix(config('cortex.foundation.route.locale_prefix') ? '{locale}/'.config('cortex.foundation.route.prefix.managerarea') : config('cortex.foundation.route.prefix.managerarea'))->group(function () {
 
-        // Testimonials Routes
+            // Testimonials Routes
              Route::name('testimonials.')->prefix('testimonials')->group(function () {
                  Route::get('/')->name('index')->uses('TestimonialsController@index');
                  Route::get('import')->name('import')->uses('TestimonialsController@import');

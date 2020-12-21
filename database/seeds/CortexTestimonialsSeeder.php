197a2652a375ca2075b9d30a0ae7bc71d0ac86ad
Abdelrahman Omran <me@omranic.com>
2020-12-22 02:24:15 +0400

    Upgrade to Laravel v8

--- database/seeds/CortexTestimonialsSeeder.php
+++ database/seeds/CortexTestimonialsSeeder.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace Cortex\Testimonials\Database\Seeders;
+
 use Illuminate\Database\Seeder;
 
 class CortexTestimonialsSeeder extends Seeder
--- routes/breadcrumbs/managerarea.php
+++ routes/breadcrumbs/managerarea.php
@@ -3,36 +3,36 @@
 declare(strict_types=1);
 
 use Cortex\Testimonials\Models\Testimonial;
-use DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs;
-use DaveJamesMiller\Breadcrumbs\BreadcrumbsGenerator;
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator;
 
-Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.index', function (BreadcrumbsGenerator $breadcrumbs) {
+Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.index', function (Generator $breadcrumbs) {
     $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.app('request.tenant')->name, route('managerarea.home'));
     $breadcrumbs->push(trans('cortex/testimonials::common.testimonials'), route('managerarea.cortex.testimonials.testimonials.index'));
 });
 
-Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.import', function (BreadcrumbsGenerator $breadcrumbs) {
+Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.import', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('managerarea.cortex.testimonials.testimonials.index');
     $breadcrumbs->push(trans('cortex/testimonials::common.import'), route('managerarea.cortex.testimonials.testimonials.import'));
 });
 
-Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.import.logs', function (BreadcrumbsGenerator $breadcrumbs) {
+Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.import.logs', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('managerarea.cortex.testimonials.testimonials.index');
     $breadcrumbs->push(trans('cortex/testimonials::common.import'), route('managerarea.cortex.testimonials.testimonials.import'));
     $breadcrumbs->push(trans('cortex/testimonials::common.logs'), route('managerarea.cortex.testimonials.testimonials.import.logs'));
 });
 
-Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.create', function (BreadcrumbsGenerator $breadcrumbs) {
+Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.create', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('managerarea.cortex.testimonials.testimonials.index');
     $breadcrumbs->push(trans('cortex/testimonials::common.create_testimonial'), route('managerarea.cortex.testimonials.testimonials.create'));
 });
 
-Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.edit', function (BreadcrumbsGenerator $breadcrumbs, Testimonial $testimonial) {
+Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.edit', function (Generator $breadcrumbs, Testimonial $testimonial) {
     $breadcrumbs->parent('managerarea.cortex.testimonials.testimonials.index');
     $breadcrumbs->push(trans('cortex/testimonials::common.testimonial', ['id' => $testimonial->getRouteKey()]), route('managerarea.cortex.testimonials.testimonials.edit', ['testimonial' => $testimonial]));
 });
 
-Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.logs', function (BreadcrumbsGenerator $breadcrumbs, Testimonial $testimonial) {
+Breadcrumbs::register('managerarea.cortex.testimonials.testimonials.logs', function (Generator $breadcrumbs, Testimonial $testimonial) {
     $breadcrumbs->parent('managerarea.cortex.testimonials.testimonials.index');
     $breadcrumbs->push(trans('cortex/testimonials::common.testimonial', ['id' => $testimonial->getRouteKey()]), route('managerarea.cortex.testimonials.testimonials.edit', ['testimonial' => $testimonial]));
     $breadcrumbs->push(trans('cortex/testimonials::common.logs'), route('managerarea.cortex.testimonials.testimonials.logs', ['testimonial' => $testimonial]));

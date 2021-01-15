486dd2b39f4ab88db5b61fb42601d906dbd69c12
Abdelrahman Omran <Omranic@users.noreply.github.com>
2021-01-15 13:41:20 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/Managerarea/TestimonialsController.php
+++ src/Http/Controllers/Managerarea/TestimonialsController.php
@@ -173,7 +173,7 @@ class TestimonialsController extends AuthorizedController
      */
     protected function form(Request $request, Testimonial $testimonial)
     {
-        if(! $testimonial->exists && $request->has('replicate') && $replicated = $testimonial->resolveRouteBinding($request->get('replicate'))){
+        if (! $testimonial->exists && $request->has('replicate') && $replicated = $testimonial->resolveRouteBinding($request->get('replicate'))) {
             $testimonial = $replicated->replicate();
         }
 

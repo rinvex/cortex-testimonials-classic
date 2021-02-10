09e33691367aa7eb7e9b92bd1df71724f4e8fc59
Abdelrahman Omran <me@omranic.com>
2021-02-11 02:58:10 +0400

    Replace form timestamps with common blade view

--- resources/views/managerarea/pages/testimonial.blade.php
+++ resources/views/managerarea/pages/testimonial.blade.php
@@ -80,7 +80,7 @@
                                         {{ Form::button(trans('cortex/testimonials::common.submit'), ['class' => 'btn btn-primary btn-flat', 'type' => 'submit']) }}
                                     </div>
 
-                                    @include('cortex/foundation::managerarea.partials.timestamps', ['model' => $testimonial])
+                                    @include('cortex/foundation::common.partials.timestamps', ['model' => $testimonial])
 
                                 </div>
 
